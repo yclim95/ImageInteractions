5f793006130ad2be3e184f949d7784d627512116
YC <lyc950421@gmail.com>
2016-05-19 19:48:21 +0800

    - Added function to change the image when button is clicked

--- app/src/main/java/com/yclim/image/MainActivity.java
+++ app/src/main/java/com/yclim/image/MainActivity.java
@@ -2,8 +2,26 @@ package com.yclim.image;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean change = true;
+
+    public void clickFunction(View view) {
+
+        ImageView catImage = (ImageView) findViewById(R.id.cat1);
+
+        if (change) {
+            change = false;
+            catImage.setImageResource(R.drawable.cat2); //Change Image
+        } else {
+            catImage.setImageResource(R.drawable.cat1);
+            change = true;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
